464a438ae15e9c6f4b2b4748fc5f7f39fc78488f
black-wolf-cyber <51896348+black-wolf-cyber@users.noreply.github.com>
2020-02-24 15:04:22 +0700

    Update number-of-0-digits-in-range.py

--- number-of-0-digits-in-range.py
+++ number-of-0-digits-in-range.py
@@ -1,1 +1,34 @@
+import math
+n,m = map(int,input().split())
+lenn = len(str(n))
+lenm = len(str(m))
 
+#code thuoc so huu cua Dang Thanh Phat
+#Hackerrank: dangthanhphat911
+#Codeforces: alexei_pc
+
+def countZeros(n):
+    if n<=0: return 0
+    result = 0
+    i = 1
+
+    while True:
+        b, c = divmod(n, i)
+        a, b = divmod(b, 10)
+
+        if a == 0:
+            return result
+
+        if b == 0:
+            result += (a - 1) * i + c + 1
+        else:
+            result += a * i
+
+        i *= 10
+        
+if n-1>0 : res = countZeros(m)-countZeros(n-1)
+elif n==0: res = countZeros(m)+1
+else: res = countZeros(m)
+
+print(res,'\n')
+    
